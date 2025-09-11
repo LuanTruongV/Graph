895d80e242c034acc9ee06898b57cd7741941796
LuanTruongV <136657015+LuanTruongV@users.noreply.github.com>
2025-09-11 10:44:24 +0700

    Update Grap.cs

--- Assets/Scripts/Grap.cs
+++ Assets/Scripts/Grap.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 
 public class Grap : MonoBehaviour
 {
-    //aaaabb
+    //aaaabbcc
     [SerializeField] private Transform _pointPrefab;
     [SerializeField][Range(10,100)] private int _resolution;
     [SerializeField]
